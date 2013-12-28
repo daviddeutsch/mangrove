588ea2dd957d192c067feae203abe372f40115b4
David Deutsch <skore@valanx.org>
2013-12-28 13:34:59 +0100

    -_-

--- pckg/mangrove-bootstrap/install.php
+++ pckg/mangrove-bootstrap/install.php
@@ -66,7 +66,7 @@ class Com_MangroveInstallerScript
 			glob($this->temp.'/install*', GLOB_ONLYDIR),
 			glob($this->temp.'/*', GLOB_ONLYDIR)
 		);
-var_dump($test);
+var_dump($test); // -_-
 		$this->mangrove = $this->temp . '/mangrove';
 
 		if ( !is_dir($this->mangrove) ) {
