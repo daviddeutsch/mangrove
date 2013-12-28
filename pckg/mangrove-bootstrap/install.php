a740c4536c0f318adf8391d01da6346601b17f13
David Deutsch <skore@valanx.org>
2013-12-28 10:12:36 +0100

    nope, debugging

--- pckg/mangrove-bootstrap/install.php
+++ pckg/mangrove-bootstrap/install.php
@@ -77,7 +77,7 @@ class Com_MangroveInstallerScript
 		// Although we have to be careful - maybe we want to update ourselves?
 
 		// Make sure we have the basic libraries in place
-
+print_r($this);exit;
 		// Load payload JSON
 		$payload = self::getJSON($this->base . '/info.json');
 
