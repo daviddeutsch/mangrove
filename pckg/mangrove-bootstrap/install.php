3b6d34672eba0ac03c90a2c65f5f041a63d009d7
David Deutsch <skore@valanx.org>
2013-12-28 15:16:46 +0100

    turns out, url was wrong

--- pckg/mangrove-bootstrap/install.php
+++ pckg/mangrove-bootstrap/install.php
@@ -87,7 +87,7 @@ class Com_MangroveInstallerScript
 			rename($this->base.'/payload/'.$zip, $this->mangrove.'/'.$zip);
 		}
 
-		// Cleanup
+		// Cleanup -_-
 		unlink($this->base);
 
 		$this->installPayload($payload, 'redbean/redbean');
