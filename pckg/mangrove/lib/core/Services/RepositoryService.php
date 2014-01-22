89e042c9280f0731e30e3783c60f5fb32ace0a7c
David Deutsch <skore@valanx.org>
2014-01-22 16:10:20 +0100

    some more work on repository directive/service

--- pckg/mangrove/lib/core/Services/RepositoryService.php
+++ pckg/mangrove/lib/core/Services/RepositoryService.php
@@ -5,5 +5,11 @@ class RepositoryService extends AbstractService
 	public function postAuthenticate()
 	{
 		// Send passphrase to server, store response as status
+		// If authenticated, also load projects, branches, packages
+	}
+
+	public function getProjects()
+	{
+		// Load project+branch list
 	}
 }
