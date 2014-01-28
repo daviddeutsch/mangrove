64a1102ef457f7edb841a656f9eaaa2173485035
David Deutsch <skore@valanx.org>
2014-01-28 03:26:35 +0100

    build trigger

--- pckg/mangrove-bootstrap/install.php
+++ pckg/mangrove-bootstrap/install.php
@@ -198,6 +198,7 @@ class Com_MangroveInstallerScript
 				break;
 
 			// Composer-style library in cms/libraries
+
 			case 'library':
 			default:
 				$path = JPATH_ROOT . '/libraries/' . $info->name;
