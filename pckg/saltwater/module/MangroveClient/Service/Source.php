b12205c2b45d41ffb1ebf78608da34ea24b44f30
David Deutsch <skore@valanx.org>
2014-05-02 11:40:04 +0200

    working on better source

---
+++ pckg/saltwater/module/MangroveClient/Service/Source.php
@@ -0,0 +1,15 @@
+<?php
+namespace MangroveClient\Service;
+
+use Saltwater\Server as S;
+use Saltwater\Thing\Service;
+
+class Source extends Service
+{
+	public function getInit()
+	{
+		$source = $this->context->data;
+
+
+	}
+}
