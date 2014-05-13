f9a5657d503282c08e6d4c2a43dd9b6336aeef18
David Deutsch <skore@valanx.org>
2014-05-14 00:56:55 +0200

    cleanup

--- pckg/aec/mi/mangrove/mangrove.php
+++ pckg/aec/mi/mangrove/mangrove.php
@@ -25,51 +25,7 @@ class mi_mangrove
 
 	function Settings()
 	{
-		$li = array();
-		$li[] = JHTML::_('select.option', 0, "--- --- ---" );
-
-		if ( !empty( $this->settings['api_key'] ) ) {
-			$MCAPI = new MCAPI( $this->settings['api_key'] );
-
-			$lists = $MCAPI->lists();
-
-			if ( !empty( $lists ) ) {
-				foreach( $lists as $list ) {
-					$li[] = JHTML::_('select.option', $list['id'], $list['name'] );
-				}
-			}
-		}
-
-		if ( !isset( $this->settings['list'] ) ) {
-			$this->settings['list'] = 0;
-		}
-
-		if ( !isset( $this->settings['list_exp'] ) ) {
-			$this->settings['list_exp'] = 0;
-		}
-
 		$settings = array();
-		$settings['explanation']	= array( 'fieldset', JText::_('MI_MI_MANGROVE_ACCOUNT_ID_NAME_NAME'), JText::_('MI_MI_MANGROVE_ACCOUNT_ID_NAME_DESC') );
-		$settings['api_key']		= array( 'inputC' );
-		$settings['account_name']	= array( 'inputC' );
-		$settings['account_id']		= array( 'inputC' );
-
-		$settings['custom_details']	= array( 'inputD' );
-
-		$settings['lists']['list']				= JHTML::_( 'select.genericlist', $li, 'list', 'size="4"', 'value', 'text', $this->settings['list'] );
-		$settings['lists']['list_unsub']		= JHTML::_( 'select.genericlist', $li, 'list_unsub', 'size="4"', 'value', 'text', $this->settings['list_unsub'] );
-		$settings['lists']['list_exp']			= JHTML::_( 'select.genericlist', $li, 'list_exp', 'size="4"', 'value', 'text', $this->settings['list_exp'] );
-		$settings['lists']['list_exp_unsub']	= JHTML::_( 'select.genericlist', $li, 'list_exp_unsub', 'size="4"', 'value', 'text', $this->settings['list_exp_unsub'] );
-
-		$settings['list']			= array( 'list' );
-		$settings['list_unsub']		= array( 'list' );
-		$settings['list_exp']		= array( 'list' );
-		$settings['list_exp_unsub']	= array( 'list' );
-		$settings['user_checkbox']	= array( 'toggle' );
-		$settings['custominfo']		= array( 'inputD' );
-
-		$rewriteswitches			= array( 'cms', 'user', 'expiration', 'subscription', 'plan', 'invoice' );
-		$settings					= AECToolbox::rewriteEngineInfo( $rewriteswitches, $settings );
 
 		return $settings;
 	}
@@ -78,34 +34,13 @@ class mi_mangrove
 	{
 		$settings = array();
 
-		$settings['user_checkbox']		= 1;
-
 		return $settings;
 	}
 
 
 	function profile_info( $request )
 	{
-		if ( !empty( $this->settings['api_key'] ) && !empty( $this->settings['account_id'] ) && !empty( $this->settings['account_name'] ) )  {
-			$MCAPI = new MCAPI( $this->settings['api_key'] );
 
-			$mcuser = $MCAPI->listMemberInfo( $this->settings['list'], $request->metaUser->cmsUser->email);
-
-			if ( empty( $mcuser['id'] ) ) {
-				$mcuser = $MCAPI->listMemberInfo( $this->settings['list_exp'], $request->metaUser->cmsUser->email);
-
-				$listid = $this->settings['list_exp'];
-			} else {
-				$listid = $this->settings['list'];
-			}
-
-			$server = explode( '-', $this->settings['api_key'] );
-
-			if ( !empty( $mcuser['id'] ) ) {
-				$message = '<p><a href="http://' . $this->settings['account_name'] . '.' . $server[1] . '.list-manage.com/unsubscribe?u=' . $this->settings['account_id'] . '&id=' . $listid . '">Unsubscribe from our newsletter</a></p>';
-				return $message;
-			}
-		}
 
 		return '';
 	}
